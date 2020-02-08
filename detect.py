d9b12903039c2b93e3154126750e7ad826c9f892
张强 <qiangzibro@gmail.com>
2020-02-08 15:08:58 +0800

    每5分钟查询一次网络

--- detect.py
+++ detect.py
@@ -9,15 +9,20 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--file', type=str, default='zm.txt')
 configs = parser.parse_args()
 
-def random_sleep(mean, std):
+
+def random_sleep(mean, std,SLEEP_NUM = 0):
+    SLEEP_NUM += 1
     time_gap = int(random.gauss(mean, std))
-    print('Time gap: ', time_gap/60, 'mins', time_gap%60, 's')
+    print("{}th sleep:{} min {} s".format(SLEEP_NUM, time_gap/60,time_gap%60))
     time.sleep(time_gap)
+    return SLEEP_NUM
 
 if __name__ == '__main__':
+    COUNTER = 0
     loginer = Login(configs.file)
     while(True):
-        if not ping(ping_url = 'www.baidu.com'):# ping fail
+        if not ping(url = 'www.baidu.com'):# ping fail
+            print("网络异常，尝试登录NBU账号")
             loginer.try_all()
-        random_sleep(300,60)
+        COUNTER = random_sleep(300,60,COUNTER)
 
